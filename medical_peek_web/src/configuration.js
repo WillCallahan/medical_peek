b6c0aaefe96d024779236c8f1bd24576d5ed17d7
WillCallahan <willcallahan94@att.net>
2020-04-18 00:34:32 -0400

    Fixes for client connection

--- medical_peek_web/src/configuration.js
+++ medical_peek_web/src/configuration.js
@@ -8,7 +8,7 @@ const devConfiguration = {
 };
 
 const prodConfiguration = {
-	medicalPeekAddress: 'https://medical-peek-api.callahanwilliam.com/v1'
+	medicalPeekAddress: 'https://medical-peek-api.callahanwilliam.com'
 };
 
 const getConfiguration = () => {
