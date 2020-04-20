d47d96fe076fcd9c26473c199ce93f216f6ca7cf
WillCallahan <willcallahan94@att.net>
2020-04-19 20:20:59 -0400

    Web - attempt to fix api url env var

--- medical_peek_web/src/configuration.js
+++ medical_peek_web/src/configuration.js
@@ -4,11 +4,11 @@ const localConfiguration = {
 };
 
 const devConfiguration = {
-	medicalPeekAddress: process.env.MEDICAL_PEEK_API_URL || 'http://127.0.0.1:8000'
+	medicalPeekAddress: process.env.REACT_APP_MEDICAL_PEEK_API_URL || 'http://127.0.0.1:8000'
 };
 
 const prodConfiguration = {
-	medicalPeekAddress: process.env.MEDICAL_PEEK_API_URL || 'https://medical-peek-api.callahanwilliam.com'
+	medicalPeekAddress: process.env.REACT_APP_MEDICAL_PEEK_API_URL || 'https://medical-peek-api.callahanwilliam.com'
 };
 
 const getConfiguration = () => {
