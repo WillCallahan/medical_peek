535dec982ac4d35f531139f1ba89470fc420351b
WillCallahan <willcallahan94@att.net>
2020-04-27 09:55:32 -0400

    Multiprocessing checkpoint
    This will not work and should be reverted

--- medical_peek_scrapper/etl/http_extractor.py
+++ medical_peek_scrapper/etl/http_extractor.py
@@ -1,7 +1,7 @@
 import logging
-from typing import Callable, List
-
 import requests
+from urllib3 import PoolManager
+from typing import Callable, List
 
 
 logger = logging.getLogger(__name__)
@@ -17,6 +17,19 @@ def scrape_product(base_url, product_id):
     response = requests.get(product_url)  # type: requests.Response
     response_text = response.text
     return product_url, response_text
+
+
+def scrape_product_thread_safe(base_url, product_id, pool: PoolManager):
+    product_url = _build_product_url(base_url, product_id)
+    logger.debug(f'Extracting product information for "{product_url}"')
+    response = pool.request('GET', product_url)
+    response_text = response.data
+    return product_url, response_text
+
+
+def scrape_all_products_thread_safe_lazy(base_url, min_product_id, max_product_id, pool: PoolManager) -> List[Callable]:
+    for product_id in range(min_product_id, max_product_id):
+        yield scrape_product_thread_safe(base_url, product_id, pool)
 
 
 def scrape_all_products_lazy(base_url, min_product_id, max_product_id) -> List[Callable]:
--- medical_peek_scrapper/main.py
+++ medical_peek_scrapper/main.py
@@ -1,11 +1,13 @@
 import logging
 import os
 
+from urllib3 import PoolManager
 from multiprocessing import Pool
 from medical_peek_core.utility.functional import thread_first, first, second, log
 from medical_peek_scrapper import settings
 from medical_peek_scrapper.etl.es_loader import load_to_elastic_search, load_to_django_store
-from medical_peek_scrapper.etl.http_extractor import scrape_product, scrape_all_products_lazy
+from medical_peek_scrapper.etl.http_extractor import scrape_product, scrape_all_products_lazy, \
+    scrape_all_products_thread_safe_lazy, scrape_product_thread_safe
 from medical_peek_scrapper.etl.html_transformer import transform_product, is_valid_product
 
 logger = logging.getLogger(__name__)
@@ -13,12 +15,15 @@ logger = logging.getLogger(__name__)
 
 def main(*args):
     logger.info('Creating thread pool')
-    pool = Pool(settings.PARALLEL_MAX_THREADS)
+    http_pool = PoolManager(1)
+    request_pool = Pool(int(settings.PARALLEL_MAX_THREADS / 2))
+    store_pool = Pool(int(settings.PARALLEL_MAX_THREADS / 2))
     thread_first(
-        scrape_all_products_lazy(settings.SCRAPE_URL, 1002982, 2000000),
+        (i for i in range(1002998, 1003000)),
+        (request_pool.map, lambda i: scrape_product_thread_safe(settings.SCRAPE_URL, i, http_pool),),
         (log, logging.INFO, 'Persisting scrapper results',),
         (filter, lambda v: is_valid_product(second(v)),),
-        (map, lambda v: load_to_django_store(first(v), second(v)),),
+        (store_pool.map, lambda v: load_to_django_store(first(v), second(v)),),
         (list,),
     )
     logger.info('Scrape complete')
